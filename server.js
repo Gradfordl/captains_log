9014a94148382f1f138572cb93a8a1fd82f78185
gradfordl <laurengradford@gmail.com>
2023-09-17 15:24:44 -0400

    Create route compl. Form data in browser as JSON.

--- server.js
+++ server.js
@@ -3,12 +3,17 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const jsxViewEngine = require("jsx-view-engine");
+const methodOverride = require("method-override");
 
 
 //Configure View Defaults
 app.set('view engine', 'jsx')
 app.set('views', './views')
 app.engine('jsx',jsxViewEngine())
+
+// Middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
 
 
 //7 Restful routes
@@ -24,6 +29,17 @@ app.get('/new', (req, res) => {
 //Update
 
 //Create
+app.post('/logs', async (req, res) => {
+    try {
+        req.body.shipIsBroken = req.body.shipIsBroken === "on"
+        // res.send("received")
+        res.send(req.body)
+    //  const createdLog = await Log.create(req.body)
+    //   res.status(201).redirect('/logs');
+    } catch (err) {
+      res.status(400).send(err);
+    }
+  });
 
 //Edit
 
--- views/New.jsx
+++ views/New.jsx
@@ -4,22 +4,24 @@ class New extends React.Component {
   render() {
     return (
       <div>
-        <h1>
-            New 
-        </h1>
+        <h1>New</h1>
         <form action="/logs" method="POST">
-           Log Title: <input type="text" name="title" /> <br/>
-           Log Entry: <input type="textarea" name="entry" /> <br/>
-           Is Ship Broken? <input type="checkbox" name="shipIsBroken"  /> <br/>
-           <input type="submit" /><br/>
-        </form> 
+          <label htmlFor="title">Title: </label>{" "}
+          <input type="text" name="title" /> <br />
+          <label htmlFor="entry">Entry:</label>{" "}
+          <textarea name="entry"></textarea>
+          <br />
+          <label htmlFor="shipIsBroken">Is Ship Broken?</label>
+           <input type="checkbox" name="shipIsBroken" /> <br />
+          <input type="submit" />
+          <br />
+        </form>
         <nav>
           <a href="/">Back to Main</a>
         </nav>
       </div>
-    )
+    );
   }
-  }
-
+}
 
 module.exports = New;
